f22159c184db88c5daf9b7f86625123c628367e4
Alissone Neyret <alissone.neyret@gmail.com>
2018-10-08 14:38:00 +0200

    inital commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Quote from "./Quote";
+import Lamp from "./lamp";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+        </header>
+        <Lamp on/>
+        <Lamp />
+        <Quote
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+        />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Quote.js
@@ -0,0 +1,14 @@
+// src/Quote.js
+import React from "react";
+
+const Quote = props => (
+  <figure>
+    <img src={props.image} alt={props.character} />
+    <figcaption>
+      <blockquote>{props.quote}</blockquote>
+      <cite>{props.character}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default Quote;

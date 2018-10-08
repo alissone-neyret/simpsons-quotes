9719903445b7be12252d1bb7c153fb23bd018eb8
Alissone Neyret <alissone.neyret@gmail.com>
2018-10-08 18:01:06 +0200

    ajout bouton travail

--- src/App.js
+++ src/App.js
@@ -6,15 +6,35 @@ import Quote from "./Quote";
 import Lamp from "./lamp";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      working: true,
+    };
+  }
+
+  auClick = () => {
+    this.setState({ working: !this.state.working });
+  };
+
   render() {
+    const work = this.state.working ? 'travail' : 'repos';
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className={`App-logo ${work}`} alt="logo" />
           <h1 className="App-title">Simpsons Quotes</h1>
         </header>
+
+        <div className="Work_button">
+        <button
+        onClick = {this.auClick}
+        className={work}>{work.toUpperCase()}</button>
+        </div>
+
         <Lamp on/>
         <Lamp />
+
         <Quote
           quote="I believe the children are the future... Unless we stop them now!"
           character="Homer Simpson"
